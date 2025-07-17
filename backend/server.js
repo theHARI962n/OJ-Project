07cb52c5eaa372da2dedd1fde713f339a9b0c9a6
theHARI962n <rajul1874@gmail.com>
2025-07-17 15:53:53 +0530

    Added login and register API

--- backend/server.js
+++ backend/server.js
@@ -28,5 +28,5 @@ app.get('/', (req, res) => {
   res.send('API is working 🚀');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
