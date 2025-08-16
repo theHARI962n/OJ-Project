74891267cc6215f6533dd704bd8f2010b12f203f
theHARI962n <rajul1874@gmail.com>
2025-08-16 11:21:08 +0530

    fix

--- frontend/src/services/AuthService.js
+++ frontend/src/services/AuthService.js
@@ -3,6 +3,8 @@ import { API_URL } from '../../api';
 
 const API = `${API_URL}/api`;
 console.log("API URL is:", API_URL);
+console.log("API URL:", process.env.REACT_APP_API_URL, "Imported API_URL:", API_URL);
+
 
 export const register = async (userData) => {
   return await axios.post(`${API}/register`, userData);
