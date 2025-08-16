9f51cbb66fb25c8d9951200f31f85bda26405fc4
theHARI962n <rajul1874@gmail.com>
2025-08-16 13:33:09 +0530

    use VITE_API_URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,2 +1,2 @@
 // api.js
-export const API_URL = process.env.REACT_APP_API_URL;
+export const API_URL = import.meta.env.VITE_API_URL;
--- frontend/src/services/AuthService.js
+++ frontend/src/services/AuthService.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { API_URL } from '../api'; 
 
 const API = `${API_URL}/api`;
-console.log("API URL is:", API_URL);
-console.log("API URL:", process.env.REACT_APP_API_URL, "Imported API_URL:", API_URL);
+console.log("API URL is:", API);
+
 
 
 export const register = async (userData) => {
